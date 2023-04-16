9581ec33ee7b543cfc91456bd8cae7178cf5d90f
Filip Condac <filipcondac@gmail.com>
2023-04-16 17:44:23 +0100

    Added form confirmation and submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
-import { SetStateAction, useState, useContext } from "react";
+import { useState, useContext } from "react";
 import { CreditCardContext } from "../context/CreditCardContext";
+import { createImageUri } from "../util";
 import {
   checkName,
   checkCardNumber,
@@ -11,12 +12,17 @@ import {
 const Form = () => {
   const creditCardContext = useContext(CreditCardContext);
 
+  //State for error messages
   const [nameError, setNameError] = useState<string>("");
   const [numberError, setNumberError] = useState<string>("");
   const [cvcError, setCvcError] = useState<string>("");
   const [expirationMMError, setExpirationMMError] = useState<string>("");
   const [expirationYYError, setExpirationYYError] = useState<string>("");
 
+  //State for form submission
+  const [formSubmitted, setFormSumbitted] = useState<boolean>(false);
+
+  //Sets all values in context
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (creditCardContext) {
       creditCardContext.setCreditCard((prevState) => ({
@@ -26,10 +32,7 @@ const Form = () => {
     }
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    console.log("valid");
-  };
-
+  //Sets name in context and checks for errors
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     creditCardContext?.setCreditCard({
       ...creditCardContext.creditCard,
@@ -43,6 +46,7 @@ const Form = () => {
     }
   };
 
+  //Sets card number in context and checks for errors
   const handleCardNumberChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -58,6 +62,7 @@ const Form = () => {
     }
   };
 
+  //Sets cvc in context and checks for errors
   const handleCvcChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     creditCardContext?.setCreditCard({
       ...creditCardContext.creditCard,
@@ -71,6 +76,7 @@ const Form = () => {
     }
   };
 
+  //Sets expirationMM in context and checks for errors
   const handleExpirationMMChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -86,6 +92,7 @@ const Form = () => {
     }
   };
 
+  // Sets expiration year error if input is invalid
   const handleExpirationYYChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -101,108 +108,183 @@ const Form = () => {
     }
   };
 
+  //Checks input for errors
+  const validateInput = () => {
+    let hasErrors = false;
+
+    if (creditCardContext) {
+      if (checkName(creditCardContext.creditCard.name)) {
+        hasErrors = true;
+      }
+      if (checkCardNumber(creditCardContext.creditCard.number)) {
+        hasErrors = true;
+      }
+      if (checkCvc(creditCardContext.creditCard.cvc)) {
+        hasErrors = true;
+      }
+      if (checkExpirationMM(creditCardContext.creditCard.expirationMM)) {
+        hasErrors = true;
+      }
+      if (checkExpirationYY(creditCardContext.creditCard.expirationYY)) {
+        hasErrors = true;
+      }
+      return hasErrors;
+    }
+  };
+
+  //Checks input for errors and sets formSubmitted to true if there are no errors
+  const handleSubmit = () => {
+    if (!validateInput()) {
+      setFormSumbitted(true);
+    }
+  };
+
+  //Resets form and context
+  const handleReset = () => {
+    if (creditCardContext) {
+      setFormSumbitted(false);
+      creditCardContext.setCreditCard({
+        number: "",
+        name: "",
+        cvc: "",
+        expirationMM: "",
+        expirationYY: "",
+      });
+    }
+  };
+
   return (
     <div className="pr-auto pl-auto m-auto mt-64 flex w-80">
-      <form action="" className="w-full">
-        <div className="m-0 mb-5">
-          <label className="mb-1 flex text-sm uppercase">Cardholder Name</label>
-          <input
-            className="flex w-full rounded border-2 p-2"
-            type="text"
-            id="name"
-            placeholder="John Doe"
-            name="name"
-            value={creditCardContext?.creditCard.name}
-            onChange={(event) => {
-              handleNameChange(event);
-              handleChange(event);
-            }}
-            maxLength={25}
-          ></input>
-          <p className="mt-2 text-xs text-red-600">{nameError}</p>
-        </div>
-
-        <div className="m-0 mb-5">
-          <label className="mb-1 flex text-sm uppercase">Card Number</label>
-          <input
-            className="flex w-full rounded border-2 p-2"
-            type="text"
-            id="number"
-            name="number"
-            onChange={(event) => {
-              handleCardNumberChange(event);
-              handleChange(event);
-            }}
-            placeholder="0000 0000 0000 0000"
-            value={creditCardContext?.creditCard.number}
-            maxLength={16}
-          ></input>
-          <p className="mt-2 text-xs text-red-600">{numberError}</p>
-        </div>
-
-        <div className="m-0 mb-5">
-          <div className="flex">
-            <label className="mb-1 text-sm uppercase">Exp. Date (MM/YY)</label>
-            <label className="mb-1 ml-10 flex text-sm uppercase">CVC</label>
+      {!formSubmitted ? (
+        <form action="" className="w-full">
+          {/** Cardholder Name */}
+          <div className="m-0 mb-5">
+            <label className="mb-1 flex text-sm uppercase">
+              Cardholder Name
+            </label>
+            <input
+              className="flex w-full rounded border-2 p-2"
+              type="text"
+              id="name"
+              placeholder="John Doe"
+              name="name"
+              value={creditCardContext?.creditCard.name}
+              onChange={(event) => {
+                handleNameChange(event);
+                handleChange(event);
+              }}
+              maxLength={25}
+            ></input>
+            <p className="mt-2 text-xs text-red-600">{nameError}</p>
           </div>
-          <div className="flex">
-            <div className="flex-col">
-              <div className="flex">
+
+          {/** Card Number */}
+          <div className="m-0 mb-5">
+            <label className="mb-1 flex text-sm uppercase">Card Number</label>
+            <input
+              className="flex w-full rounded border-2 p-2"
+              type="text"
+              id="number"
+              name="number"
+              onChange={(event) => {
+                handleCardNumberChange(event);
+                handleChange(event);
+              }}
+              placeholder="0000 0000 0000 0000"
+              value={creditCardContext?.creditCard.number}
+              maxLength={16}
+            ></input>
+            <p className="mt-2 text-xs text-red-600">{numberError}</p>
+          </div>
+
+          {/** Expiration Date and CVC */}
+          <div className="m-0 mb-5">
+            <div className="flex">
+              <label className="mb-1 text-sm uppercase">
+                Exp. Date (MM/YY)
+              </label>
+              <label className="mb-1 ml-10 flex text-sm uppercase">CVC</label>
+            </div>
+            <div className="flex">
+              <div className="flex-col">
+                <div className="flex">
+                  <input
+                    className="mr-3 inline-flex w-16 rounded border-2 p-2"
+                    type="text"
+                    placeholder="MM"
+                    maxLength={2}
+                    onChange={(event) => {
+                      handleExpirationMMChange(event);
+                      handleChange(event);
+                    }}
+                    name="expirationMM"
+                    value={creditCardContext?.creditCard.expirationMM}
+                  ></input>
+                  <input
+                    className="inline-flex w-16 rounded border-2 p-2"
+                    type="text"
+                    placeholder="YY"
+                    maxLength={2}
+                    onChange={(event) => {
+                      handleExpirationYYChange(event);
+                      handleChange(event);
+                    }}
+                    name="expirationYY"
+                    value={creditCardContext?.creditCard.expirationYY}
+                  ></input>
+                </div>
+                <div className="mt-2 text-xs text-red-600">
+                  {expirationMMError ||
+                    (expirationYYError && "Invalid exp. date")}
+                </div>
+              </div>
+              <div className="flex-col">
                 <input
-                  className="mr-3 inline-flex w-16 rounded border-2 p-2"
+                  className="ml-2 w-full rounded border-2 p-2"
                   type="text"
-                  placeholder="MM"
-                  maxLength={2}
+                  placeholder="123"
+                  maxLength={3}
                   onChange={(event) => {
-                    handleExpirationMMChange(event);
+                    handleCvcChange(event);
                     handleChange(event);
                   }}
-                  name="expirationMM"
-                  value={creditCardContext?.creditCard.expirationMM}
+                  name="cvc"
+                  value={creditCardContext?.creditCard.cvc}
                 ></input>
-                <input
-                  className="inline-flex w-16 rounded border-2 p-2"
-                  type="text"
-                  placeholder="YY"
-                  maxLength={2}
-                  onChange={(event) => {
-                    handleExpirationYYChange(event);
-                    handleChange(event);
-                  }}
-                  name="expirationYY"
-                  value={creditCardContext?.creditCard.expirationYY}
-                ></input>
-              </div>
-              <div className="mt-2 text-xs text-red-600">
-                {expirationMMError ||
-                  (expirationYYError && "Invalid exp. date")}
+                <p className="ml-7 mt-2 text-xs text-red-600">{cvcError}</p>
               </div>
             </div>
-            <div className="flex-col">
-              <input
-                className="ml-6 inline-flex w-full rounded border-2 p-2"
-                type="text"
-                placeholder="123"
-                maxLength={3}
-                onChange={(event) => {
-                  handleCvcChange(event);
-                  handleChange(event);
-                }}
-                name="cvc"
-                value={creditCardContext?.creditCard.cvc}
-              ></input>
-              <p className="ml-7 mt-2 text-xs text-red-600">{cvcError}</p>
-            </div>
+
+            {/** Submit Button */}
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="mt-5 w-full rounded-md bg-violet-900 p-3 text-white"
+            >
+              Confirm
+            </button>
           </div>
+        </form>
+      ) : (
+        <div className="m-auto flex flex-col">
+          <img
+            src={createImageUri("/images/icon-complete.svg")}
+            alt="checkmark of completed form"
+            className="m-auto mb-10 w-20"
+          />
+          <h2 className="m-auto text-4xl">THANK YOU!</h2>
+          <p className=" m-auto mt-5 text-center text-gray-400">
+            We've added your card details{" "}
+          </p>
           <button
             type="button"
-            onClick={handleSubmit}
-            className="mt-5 w-full rounded-md bg-violet-900 p-3 text-white"
+            onClick={handleReset}
+            className="mt-16 w-full rounded-md bg-violet-900 p-3 text-white"
           >
             Confirm
           </button>
         </div>
-      </form>
+      )}
     </div>
   );
 };
